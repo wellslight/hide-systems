dd8f1b8350c04512ae5ed9893bd53d0a75dee8cd
wellslight <keeslerda@gmail.com>
2017-02-27 15:23:28 -0600

    Add files via upload

---
+++ hidesystem.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mv /home/pi/RetroPie/roms/nes /home/pi/RetroPie/roms/hiddensystems/
+sudo mv /home/pi/RetroPie/roms/restoresystems/restorenes.sh /home/pi/RetroPie/retropiemenu/systems/restorenes.sh
+sudo reboot
---
+++ restorenes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mv /home/pi/RetroPie/roms/hiddensystems/nes /home/pi/RetroPie/roms/nes
+sudo mv /home/pi/RetroPie/retropiemenu/systems/restorenes.sh /home/pi/RetroPie/roms/restoresystem/restorenes.sh
+sudo reboot
